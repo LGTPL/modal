d4601dc934d186203e10d91b4c4d2c0cef04d380
LGTPL <168435813+LGTPL@users.noreply.github.com>
2026-01-29 10:21:07 +0530

    Create toast.js

---
+++ toast.js
@@ -0,0 +1,33 @@
+function showToast(message = "Success!", duration = 3000) {
+
+  const container = document.getElementById("toast-container");
+
+  if (!container) return;
+
+  const toast = document.createElement("div");
+  toast.className = "toast";
+
+  toast.innerHTML = `
+    <div class="toast-icon">✓</div>
+    <div>${message}</div>
+    <div class="toast-close">✕</div>
+  `;
+
+  container.appendChild(toast);
+
+  toast.querySelector(".toast-close").onclick = () => {
+    hideToast(toast);
+  };
+
+  setTimeout(() => {
+    hideToast(toast);
+  }, duration);
+}
+
+function hideToast(toast) {
+  toast.classList.add("hide");
+
+  setTimeout(() => {
+    toast.remove();
+  }, 300);
+}
