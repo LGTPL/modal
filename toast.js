854b76270d5af11604d0deeec3e99d541262019e
LGTPL <168435813+LGTPL@users.noreply.github.com>
2026-01-29 12:32:40 +0530

    Update toast.js

--- toast.js
+++ toast.js
@@ -1,4 +1,4 @@
-function showToast(message = "Success!", duration = 3000) {
+function showToast(message = "Success!", duration = 60000) {
 
   const container = document.getElementById("toast-container");
 
